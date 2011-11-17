62fff09089761007f67ee077bfdbcfd7d39e0b0b
Konstantin Sorokin <kvs@sigterm.ru>
2011-11-17 20:30:57 +0400

    Modified test.

--- readline_test.py
+++ readline_test.py
@@ -3,14 +3,11 @@
 
 import readline
 
-#values = ['aaa0',  'aaa1',  'aaa2',  'bbb_0', 'bbb_1', 'bbb_2',
-#          'ccc-0', 'ccc-1', 'ccc-2', 'ddd?0', 'ddd?1', 'ddd?2']
+values = [['show', 'add', 'delete', 'tracking', 'save'], ['events', 'delete']]
 
-level1_values = ['show', 'add', 'delete', 'tracking', 'save']
-level2_values = ['events', 'delete']
 
-def level1_completer(text, state):
-    matches = [v for v in level1_values if v.startswith(text)]
+def completer(text, state):
+    matches = [v for v in values[level] if v.startswith(text)]
     if len(matches) == 1 and matches[0] == text:
         # Add space if the current text is the same as the only match
         return "{} ".format(matches[0]) if state == 0 else None
@@ -21,41 +18,20 @@ def level1_completer(text, state):
     return matches[state]
 
 
-def level2_completer(text, state):
-    matches = [v for v in level2_values if v.startswith(text)]
-    if len(matches) == 1 and matches[0] == text:
-        # Add space if the current text is the same as the only match
-        return "{} ".format(matches[0]) if state == 0 else None
-
-    if state >= len(matches):
-        return None
-
-    return matches[state]
-
-
-def level1_parse_and_exec(cmd):
-    cmd_parts = cmd.split()
-    command = cmd_parts[0].lower()
-
-    if len(cmd_parts) > 1:
-        arg = cmd_parts[1]
-    else:
-        arg = None
-    
+def parse_and_exec_0(command, arg):
     if command == 'tracking' and arg:
-
-        saved_completer = readline.get_completer()
-        readline.set_completer(level2_completer)
+        global level
+        level = 1
 
         while True:
             try:
                 cmd = raw_input("  " + arg + "# ")
-                level2_parse_and_exec(cmd)
+                parse_and_exec(cmd)
             except EOFError:
+                print
                 break
         
-        readline.set_completer(saved_completer)
-
+        level = 0
     elif command == 'show':
         print "Executing 'show' command"
     elif command == 'save':
@@ -68,8 +44,23 @@ def level1_parse_and_exec(cmd):
         print "Unknown command and arg: (%s, %s)" % (command, arg)
 
 
-def level2_parse_and_exec(cmd):
-    print "Level2: %s" % cmd
+def parse_and_exec_1(command, arg):
+    print "Level2: cmd=%s, arg=%s" % (command, arg)
+
+
+def parse_and_exec(cmd):
+    cmd_parts = cmd.split()
+    command = cmd_parts[0].lower()
+
+    if len(cmd_parts) > 1:
+        arg = cmd_parts[1]
+    else:
+        arg = None
+
+    if level == 0:
+        parse_and_exec_0(command, arg)
+    else:
+        parse_and_exec_1(command, arg)
 
 
 def main():    
@@ -77,15 +68,20 @@ def main():
     for line in ("tab: complete", "set show-all-if-unmodified on"):
         readline.parse_and_bind(line)
 
-    readline.set_completer(level1_completer)
+    readline.set_completer(completer)
+
+    global level
+    level = 0
 
     while True:
         try:
             cmd = raw_input("parcelwatch> ")
             if len(cmd) == 0: continue
-            level1_parse_and_exec(cmd)
+            parse_and_exec(cmd)
         except EOFError:
+            print
             break
+
 
 if __name__ == '__main__':
     main()
