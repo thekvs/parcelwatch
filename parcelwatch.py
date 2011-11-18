8fe1fca7c84b6825c6a2bfa9994fe460ac44ef60
Konstantin Sorokin <kvs@sigterm.ru>
2011-11-18 17:10:54 +0400

    Use configuration file.
    All needed configuration parameters now specified through configuration file.

--- parcelwatch.py
+++ parcelwatch.py
@@ -9,6 +9,7 @@ import shutil
 import pickle
 
 from optparse import OptionParser
+from ConfigParser import SafeConfigParser
 
 from notifications.sms import ComtubeRuSMS
 from parsers.russianpost import RussianPostQuery
@@ -21,74 +22,79 @@ def parse_args():
     parser.add_option("", "--shell", action="store_true",
         dest="shell", default=False,
         help="run parcelwatch's shell for admin tasks")
-    parser.add_option("", "--data-file", action="store",
-        dest="data_file", help="file to store state between checks",
+    parser.add_option("", "--config", action="store",
+        dest="config", help="configuration file",
         metavar="FILE")
-    parser.add_option("", "--user", action="store",
-        dest="user", help="comtube's service user name")
-    parser.add_option("", "--password", action="store",
-        dest="password", help="comtube's service user's password")
-    parser.add_option("", "--mobile", action="store",
-        dest="mobile", help="number of mobile phone to send sms to")
-
+    
     opts, args = parser.parse_args()
 
-    if opts.shell and opts.data_file:
-        return opts
-    elif not (opts.data_file and opts.user and opts.password and opts.mobile):
+    if not opts.config:
         parser.error("Not enough args. given. Try --help switch for details.")
 
     return opts
 
 
-def save_cache(opts, cache):
+def save_cache(conf, cache):
+    data_file = conf.get("status", "file")
     tmp_data_file = tempfile.NamedTemporaryFile(delete=False)
     pickle.dump(cache, tmp_data_file)
-    shutil.move(tmp_data_file.name, opts.data_file)
+    shutil.move(tmp_data_file.name, data_file)
 
 
-def load_cache(opts):
-    if os.path.exists(opts.data_file):
-        data_file = open(opts.data_file, "r")
-        cache = pickle.load(data_file)
+def load_cache(conf):
+    data_file = conf.get("status", "file")
+    if os.path.exists(data_file):
+        cache = pickle.load(open(data_file, "r"))
     else:
         cache = dict()
 
     return cache
 
 
+def parse_config(config):
+    conf = SafeConfigParser()
+    conf.readfp(open(config))
+
+    return conf
+
+
 def main():
     opts = parse_args()
-    cache = load_cache(opts)
+    conf = parse_config(opts.config)
 
-    if opts.shell:
+    cache = load_cache(conf)
+
+    if not opts.shell:
+        handle = RussianPostQuery()
+
+        user = conf.get("sms", "comtube_user")
+        password = conf.get("sms", "comtube_password")
+        mobile = conf.get("notifications", "mobile")
+
+        sms = ComtubeRuSMS(user, password, mobile)
+
+        for identifier in cache.iterkeys():
+            events = handle.query(identifier.strip())
+            events_count = len(events)
+
+            if cache.has_key(identifier):
+                cached_events = cache[identifier]
+                cached_events_count = len(cached_events)
+                if events_count > 0 and events_count > cached_events_count:
+                    new_events_count = events_count - cached_events_count
+                    first = cached_events_count
+                    last = cached_events_count + new_events_count
+                    for idx in xrange(first, last):
+                        event = events[idx]
+                        cached_events.append(event)
+                        msg = "Отправление %s: %s" % (identifier, str(event))
+                        code, status = sms.send(msg)
+            else:       
+                cache[identifier] = events
+    else:
         run_shell(cache)
-        save_cache(opts, cache)
-        return
 
-    handle = RussianPostQuery()
-    sms = ComtubeRuSMS(opts.user, opts.password, opts.mobile)
-
-    for identifier in cache.iterkeys():
-        events = handle.query(identifier.strip())
-        events_count = len(events)
-
-        if cache.has_key(identifier):
-            cached_events = cache[identifier]
-            cached_events_count = len(cached_events)
-            if events_count > 0 and events_count > cached_events_count:
-                new_events_count = events_count - cached_events_count
-                first = cached_events_count
-                last = cached_events_count + new_events_count
-                for idx in xrange(first, last):
-                    event = events[idx]
-                    cached_events.append(event)
-                    msg = "Отправление %s: %s" % (identifier, str(event))
-                    code, status = sms.send(msg)
-        else:       
-            cache[identifier] = events
-
-    save_cache(opts, cache)
+    save_cache(conf, cache)
     
 
 if __name__ == '__main__':
